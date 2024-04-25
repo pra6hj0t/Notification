2391b1f0023474f8abcb3e24a90116fbec59ac4d
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-04-25 08:55:30 +0530

    data

---
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.notifications
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "Null"
+    val CHANNEL_NAME = "Null"
+    val notificationId = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      //  enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        createNotificationChannel()
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("Hello Prabh")
+            .setContentText("Notification is working")
+            .setSmallIcon(R.drawable.baseline_insert_emoticon_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        val btn = findViewById<Button>(R.id.button)
+        val nManager = NotificationManagerCompat.from(this)
+
+        btn.setOnClickListener {
+
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(this,"Please enable Notify Permission",Toast.LENGTH_SHORT).show()
+            }
+            nManager.notify(notificationId,builder)
+        }
+
+    }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "This is my Notification"
+            }
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
